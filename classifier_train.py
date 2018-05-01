4ba7ce54be79b625bf201d32e364bef6f24e1470
AISangam <you@example.com>
2018-05-02 00:57:36 +0530

    Initial Commit

---
+++ classifier_train.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import sys
+
+from packages.classifier import training
+
+datadir = './pre_img'
+modeldir = ''
+classifier_filename = './class/classifier.pkl'
+print ("Training Start")
+obj=training(datadir,modeldir,classifier_filename)
+get_file=obj.main_train()
+print('Saved classifier model to file "%s"' % get_file)
+sys.exit("All Done")
